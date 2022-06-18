df0e0cf1e8b8b234ef93bfa31b9903c1443b372f
Brian Pang <brianpang@yahoo.com>
2022-06-17 22:40:35 -0400

    Updates

---
+++ precache-manifest.87ade6e66e7354d66b703f327b26b27e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29c552f62cfebc8f4490",
+    "url": "/my-portfolio/static/css/main.d1f13185.chunk.css"
+  },
+  {
+    "revision": "29c552f62cfebc8f4490",
+    "url": "/my-portfolio/static/js/main.29c552f6.chunk.js"
+  },
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my-portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "68a8e28dad5f177fe761dfe1442901be",
+    "url": "/my-portfolio/index.html"
+  }
+];
